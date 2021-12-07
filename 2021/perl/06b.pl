9b96087f3d3469130da29b25f97ce161de970084
Yoshi Takahashi <yoshi@acm.org>
2021-12-06 17:30:23 -0800

    a little cleanup

--- 2021/perl/06b.pl
+++ 2021/perl/06b.pl
@@ -17,23 +17,20 @@ for( my $n = 0; $n <= 8; $n++ ) {
 for( my $i = 1; $i <= 256; $i++ ) {
 	my %gen = %fishies;
 
-	my $regen = 0;
-	my $spawn = 0;
-	for( my $fish_gen = 0; $fish_gen <= 8; $fish_gen++) {
-		if( $fish_gen == 0 ) {
-			$spawn += $fishies{0};
-			$regen += $fishies{0};
-		} else {
-			$gen{$fish_gen - 1} = $fishies{$fish_gen};
-		}
+	for( my $fish_gen = 1; $fish_gen <= 8; $fish_gen++) {
+		$gen{$fish_gen - 1} = $fishies{$fish_gen};
 	}
+
+	my $regen = $fishies{0};
 	$gen{6} += $regen;
-	$gen{8} = $spawn;
+	$gen{8} = $regen;
 
 	%fishies = %gen;
 }
+
 my $fish_count = 0;
 foreach my $fish_gen (keys %fishies ) {
 	$fish_count += $fishies{$fish_gen};
 }
+
 print qq(fishies: $fish_count\n);
