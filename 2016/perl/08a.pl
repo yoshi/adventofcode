4e3da1fb9f4ed19a3315055c4161bda494713f0a
Yoshi Takahashi <yoshi@acm.org>
2024-11-20 22:47:50 -0800

    partial day 8a

--- 2016/perl/08a.pl
+++ 2016/perl/08a.pl
@@ -2,11 +2,65 @@
 
 my $count = 0;
 
+my @screen;
+
+for( my $i = 0; $i < 5; $i++ ) {
+	push @screen, "." x 50;
+}
+
+&print_screen;
+
 chomp( my @dat = <> );
 
 foreach $line ( @dat ) {
+	print $line . "\n";
+	my @op = split( " ", $line );
+
+	if( $op[0] eq "rect" ) {
+		# this is a rect instruction
+		my( $width, $height ) = split( "x", $op[1] );
+		print "this is a create rect instruction $width $height\n";
+		for( my $i = 0; $i < $height; $i++ ) {
+			substr( $screen[$i], 0, $width ) = "#" x $width;
+		}
+	} elsif( $op[0] eq "rotate" ) {
+		if( $op[1] eq "row" ) {
+			# this is a rotate row instruction
+			my( $row, $steps ) = (join( " ", @op[2,3,4]) =~ /y=([0-9]+) by ([0-9]+)/);
+			print "row $row steps $steps\n";
+			my @working_row = split( "", $screen[$row] );
+			for( my $i = 0; $i < $steps; $i++ ){
+				my $c = pop( @working_row );
+				unshift( @working_row, $c );
+			}
+#			print "wr: " . join("", @working_row) . "\n";
+			$screen[$row] = join("", @working_row);
+		} else {
+			# this is a rotate column instruction
+			my( $col, $steps ) = (join( " ", @op[2,3,4]) =~ /x=([0-9]+) by ([0-9]+)/);
+			my @working_col;
+
+			for( my $i = 0; $i < 5; $i++ ) {
+				push( @working_col, substr($screen[$i], $col, 1));
+			}
+			print "before wc: " . join("", @working_col) . "\n";
+			for( my $i = 0; $i < $steps; $i++ ) {
+				my $c = pop( @working_col );
+				unshift( @working_col, $c );
+			}
+			print "after  wc: " . join("", @working_col) . "\n";
+			for( my $i = 0; $i < 5; $i++ ) {
+				substr($screen[$i], $col, 1) = $working_col[$i];
+			}
+		}
+	}
+	&print_screen;
 }
 
 print "count: $count\n";
 
 exit( 0 );
+
+sub print_screen {
+	print join( "\n", @screen ) . "\n";
+}
