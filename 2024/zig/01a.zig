b4c82ccf02cf46bd4affa5b8ed2a9662604168ac
Yoshi Takahashi <yoshi@acm.org>
2025-12-01 19:17:33 -0800

    cleanup

--- 2024/zig/01a.zig
+++ 2024/zig/01a.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub fn main() !void {
+    const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Hi.\n", .{});
 }
